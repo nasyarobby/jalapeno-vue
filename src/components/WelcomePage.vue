febf472ef3759bd8f02383a73fe2679249093cea
Nasyarobby <nasyarobby@gmail.com>
2019-07-17 22:08:49 -0400

    added mock page for welcome page.

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,5 +1,117 @@
 <template>
-  <div>Helo</div>
+  <v-container fluid grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1 class="headline">Cookbooks Collection</h1>
+      </v-flex>
+      <v-flex v-for="cookbook in this.dummy.cookbooks" :key="cookbook.id" xs12 sm6 md4 lg3>
+        <v-card>
+          <v-img
+            class="white--text"
+            height="200px"
+            :src="`https://unsplash.it/600/300?image=${Math.floor(Math.random() * 100) + 1}`"
+          >
+            <v-layout pa-0 ma-0 row fill-height align-end class="lightbox white--text">
+              <v-flex xs12 class="text-xs-center titlebg">
+                <h3 class="headline mb-0">
+                  <router-link
+                    class="white--text"
+                    style="text-decoration:none;"
+                    :to="`/users/${username}/cookbooks/${cookbook.id}`"
+                  >{{cookbook.cookbook_name}}</router-link>
+                </h3>
+              </v-flex>
+            </v-layout>
+          </v-img>
+
+          <v-card-text>
+            <v-chip color="pink" text-color="white">
+              <v-icon left>label</v-icon>
+              {{cookbook.category}}
+            </v-chip>
+            <v-chip>
+              <v-avatar>
+                <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="trevor" />
+              </v-avatar>
+              {{cookbook.owner}}
+            </v-chip>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="light-blue" dark>
+              <v-icon>book</v-icon>
+              {{cookbook.numOfRecipes }} recipes
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn icon @click="love">
+              <v-icon>favorite</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>bookmark</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>share</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+<style scoped>
+.titlebg {
+  background: rgba(70, 70, 70, 0.5);
+}
+</style>
 <script>
+export default {
+  data() {
+    return {
+      username: this.$route.params.username,
+      dummy: {
+        cookbooks: [
+          {
+            id: 1,
+            user_id: 1,
+            owner: "Robby D",
+            cookbook_name: "Spend with Pennies",
+            category: "Salads",
+            numOfRecipes: 1
+          },
+          {
+            id: 2,
+            user_id: 1,
+            owner: "Robby D",
+            cookbook_name: "Cooking Classy",
+            category: "Drinks",
+            numOfRecipes: 25
+          },
+          {
+            id: 3,
+            user_id: 1,
+            owner: "Robby D",
+            cookbook_name: "Deserted Dessert",
+            category: "Dessert",
+            numOfRecipes: 10
+          },
+          {
+            id: 4,
+            user_id: 1,
+            owner: "Robby D",
+            cookbook_name: "My Cookbook",
+            category: "Cookbook",
+            numOfRecipes: 3
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    dummyClick() {
+      alert("Ets");
+    },
+    love() {
+      alert("Love");
+    }
+  }
+};
 </script>
