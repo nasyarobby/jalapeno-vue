014aedb368d37ceb1c8dd73e907a7d99e1b82918
Nasyarobby <nasyarobby@gmail.com>
2019-08-08 11:40:50 -0400

    added validations.

--- src/components/RecipeEdit.vue
+++ src/components/RecipeEdit.vue
@@ -7,12 +7,31 @@
       <v-flex xs12 sm10 offset-sm1 lg8 offset-lg2>
         <v-form ref="form">
           <v-text-field v-model="recipe.name" :rules="rules.name" label="Recipe Name" required></v-text-field>
-          <v-textarea v-model="recipe.description" label="Description" required auto-grow rows="1"></v-textarea>
-          <v-textarea v-model="recipe.directions" label="Directions" required auto-grow rows="3"></v-textarea>
-          <v-textarea v-model="recipe.notes" label="Notes" required auto-grow rows="1"></v-textarea>
+          <v-textarea
+            v-model="recipe.description"
+            :rules="rules.description"
+            label="Description"
+            required
+            auto-grow
+            rows="1"
+          ></v-textarea>
+          <v-textarea
+            v-model="recipe.directions"
+            :rules="rules.directions"
+            label="Directions"
+            required
+            auto-grow
+            rows="3"
+          ></v-textarea>
+          <v-textarea v-model="recipe.notes" label="Notes" auto-grow rows="1"></v-textarea>
           <v-layout wrap>
             <v-flex xs6 sm4 md3 lg2 pr-3>
-              <v-text-field v-model="recipe.preparationTime" label="Preparation Time" mask="####"></v-text-field>
+              <v-text-field
+                v-model="recipe.preparationTime"
+                :rules="rules.prepTime"
+                label="Preparation Time"
+                mask="####"
+              ></v-text-field>
             </v-flex>
             <v-flex xs6 sm4 md3 lg2>
               <v-select v-model="recipe.preparationTimeUnit" :items="units"></v-select>
@@ -20,7 +39,12 @@
           </v-layout>
           <v-layout wrap>
             <v-flex xs6 sm4 md3 lg2 pr-3>
-              <v-text-field v-model="recipe.cookTime" label="Cook Time" mask="####"></v-text-field>
+              <v-text-field
+                v-model="recipe.cookTime"
+                :rules="rules.cookTime"
+                label="Cook Time"
+                mask="####"
+              ></v-text-field>
             </v-flex>
             <v-flex xs6 sm4 md3 lg2>
               <v-select v-model="recipe.cookTimeUnit" :items="units"></v-select>
@@ -28,22 +52,31 @@
           </v-layout>
           <v-layout wrap>
             <v-flex xs6 sm4 md3 lg2 pr-3>
-              <v-text-field v-model="recipe.portions" label="Portions" mask="####"></v-text-field>
+              <v-text-field
+                v-model="recipe.portions"
+                :rules="rules.portions"
+                label="Portions"
+                mask="####"
+              ></v-text-field>
             </v-flex>
           </v-layout>
           <h5 class="subheading">Ingredients</h5>
           <v-layout wrap v-for="(ing, index) in recipe.ingredients" v-bind:key="index ">
-            <v-flex xs6 sm4 md3 lg3 pr-3>
-              <v-text-field v-model="ing.name" label="Ingredients Name"></v-text-field>
-            </v-flex>
-            <v-flex xs6 sm4 md3 lg1 pr-3>
-              <v-text-field v-model="ing.quantity" label="Quantity"></v-text-field>
-            </v-flex>
-            <v-flex xs6 sm4 md3 lg1 pr-3>
-              <v-text-field v-model="ing.quantity_text" label="Quantity Display"></v-text-field>
+            <v-flex xs6 sm4 md3 lg2 pr-3>
+              <v-text-field v-model="ing.name" :rules="rules.required" label="Ingredients Name"></v-text-field>
             </v-flex>
             <v-flex xs6 sm4 md3 lg2 pr-3>
-              <v-text-field v-model="ing.unit" label="Unit"></v-text-field>
+              <v-text-field v-model="ing.quantity" :rules="rules.required" label="Quantity"></v-text-field>
+            </v-flex>
+            <v-flex xs6 sm4 md3 lg2 pr-3>
+              <v-text-field
+                v-model="ing.quantity_text"
+                :rules="rules.required"
+                label="Quantity Display"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs6 sm4 md3 lg2 pr-3>
+              <v-text-field v-model="ing.unit" :rules="rules.required" label="Unit"></v-text-field>
             </v-flex>
             <v-flex align-self-center>
               <v-btn color="success" small fab flat @click="addIngredient(index)">
@@ -62,7 +95,7 @@
             </v-flex>
             <v-flex align-self-center>{{ing.quantity_text}} {{ing.unit}} {{ing.name}}</v-flex>
           </v-layout>
-          <v-btn color="success" @click="submit">Update</v-btn>
+          <v-btn color="success" @click="submit">Create</v-btn>
         </v-form>
       </v-flex>
     </v-layout>
@@ -89,8 +122,29 @@ export default {
         categories: [2]
       },
       rules: {
-        name: [v => !!v || "Name is required"],
-        category: [v => !!v || "category is required"]
+        name: [v => !!v.trim() || "Name is required"],
+        category: [v => !!v.trim() || "category is required"],
+        description: [v => !!v.trim() || "Description is required."],
+        directions: [
+          v =>
+            !!v.trim() || "What cooking recipes do not have any directions??!!"
+        ],
+        cookTime: [
+          v => {
+            return !!String(v).trim() || "Cooking time is required.";
+          }
+        ],
+        prepTime: [
+          v => {
+            return !!String(v).trim() || "Preparation time is required.";
+          }
+        ],
+        portions: [
+          v => {
+            return !!String(v).trim() || "Portions is required.";
+          }
+        ],
+        required: [v => !!String(v).trim() || "Required"]
       }
     };
   },
